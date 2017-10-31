899d0239ac5093545306bc689a38b606d2784c45
alexander-liao <alex.yj.liao@gmail.com>
2017-10-30 23:00:29 -0400

    added plotter

--- dev/test_code/outlier_remover.py
+++ dev/test_code/outlier_remover.py
@@ -1,8 +1,29 @@
 import numpy
+import json
+import prettyprint
+
+with open("test_configs.json", "r") as f:
+    settings = json.loads(f.read())
 
 data = []
 
-with open("data.txt", "r") as f:
+def get(name, default = None):
+    sub = settings
+    for part in name.strip().split("."):
+        part = part.strip()
+        if part in sub:
+            sub = sub[part]
+        else:
+            break
+    else:
+        return sub
+    return default
+
+in_file = get("io.filename", "data.txt")
+deviations = get("algo.deviations", 2)
+maxcycles = get("algo.maxcycles", -1)
+
+with open(in_file, "r") as f:
     for line in f.readlines():
         data.append(list(map(float, line.strip().split())))
 
@@ -13,11 +34,17 @@ def ofilter(matrix):
     dels = []
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
-            if abs(matrix[row][col] - means[col]) > 2 * stds[col]:
+            if abs(matrix[row][col] - means[col]) > deviations * stds[col]:
                 dels.append(row)
     return [e for i, e in enumerate(matrix) if i not in dels]
 
-print(data)
-filtered = ofilter(data)
-print(filtered)
-print(len(filtered))
+old = []
+cycles = 0
+while cycles < maxcycles and old != data:
+    old = data
+    data = ofilter(data)
+    cycles += 1
+# print(data)
+print(prettyprint.plot([row[0] for row in data], 0.1))
+# print(len(data))
+# print(cycles)
